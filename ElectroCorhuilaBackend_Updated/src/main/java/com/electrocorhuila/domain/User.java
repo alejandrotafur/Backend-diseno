d1718318261e7c666716d74ccaf48be5d15d7965
Alejandro Tafur jojoa <atafur-2023a@corhuila.edu.co>
2025-04-30 01:10:01 -0500

    Agregando documentación PDF

---
+++ ElectroCorhuilaBackend_Updated/src/main/java/com/electrocorhuila/domain/User.java
@@ -0,0 +1,14 @@
+package com.electrocorhuila.domain;
+
+public class User {
+    private Long id;
+    private String name;
+    private String email;
+    private int socioeconomicStratum;
+
+    public int getSocioeconomicStratum() {
+        return 0;
+    }
+
+    // Getters and Setters
+}
